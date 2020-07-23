0b06f5d89415fb67c4e86df3164be558fbacbad7
jasonkovalchick <68664481+jasonkovalchick@users.noreply.github.com>
2020-07-23 14:12:05 -0400

    Version 1.0.3
    Version 1.0.3
    
    -populated the about and contact pages with basic content, subpages to come soon
    -added quick and simple animations to moving between some pages
    -changed CSS to work better on mobile, text gets larger and menu goes vertical
    -other small changes

---
+++ script.js
@@ -0,0 +1,8 @@
+const swup = new Swup();
+
+const options = {
+    linkSelector:
+      'a[href^="' +
+      window.location.origin +
+      '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])'
+  };
